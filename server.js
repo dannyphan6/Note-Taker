d08af091738672f945f155780c18df838e148a6f
Daniel Phan <dannyphan@comcast.net>
2021-04-18 14:13:54 -0700

    Added comment

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const fs = require("fs");
 
 const app = express();
 const PORT = 5000;
+
+// Allows you to use all files in the "public directory"
+app.use(express.static("public"));
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
