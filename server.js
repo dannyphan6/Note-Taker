27df3e8499acf5f108d6e74becfec7f3dbea4280
Daniel Phan <dannyphan@comcast.net>
2021-04-18 14:42:06 -0700

    Set up route for notes

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path")
+const htmlDir = path.join(__dirname, "/public")
 
 const app = express();
-const PORT = 5000;
+let PORT = process.env.PORT || 5000;
 
 // Allows you to use all files in the "public directory"
 app.use(express.static("public"));
-
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.get("/notes", (req, res) => res.sendFile(path.join(htmlDir, "notes.html")));
 
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
