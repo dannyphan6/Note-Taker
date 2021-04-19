025e89f76d39af506b9cb5bb4b65137d1263053a
Daniel Phan <dannyphan@comcast.net>
2021-04-18 21:35:03 -0700

    Created a route for HTML webpage

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.get("/", (req, res) => res.sendFile(path.join(mainDir, "index.html")));
+
 app.get("/notes", (req, res) => res.sendFile(path.join(mainDir, "notes.html")));
 
 app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "/db/db.json")));
