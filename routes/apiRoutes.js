2dfb6a849b016389f6beb18b109a765cf12fc106
Daniel Phan <dannyphan@comcast.net>
2021-04-21 00:12:16 -0700

    Fixed issue with notes route not working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,17 +3,17 @@ const path = require("path")
 const uuid = require('uuid');
 
 module.exports = app => {
-    app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "/db/db.json")));
+    app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "../db/db.json")));
     
     app.get("/api/notes/:id", (req, res) => {
-        const saveNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        const saveNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
     
         // 
         res.json(saveNotes[Number(req.params.id)]);
     });
     
     app.post("/api/notes", (req, res) => {
-        const storeNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+        const storeNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
         
         // user data is being accessed from the fetch in index.js 
         const incomingNote = req.body;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,8 @@
 const path = require("path")
-const mainDir = path.join(__dirname, "/public")
+// const mainDir = path.join(__dirname, "/public")
 
 module.exports = app => {
     app.get("/", (req, res) => res.sendFile(path.join(mainDir, "index.html")));
     
-    app.get("/notes", (req, res) => res.sendFile(path.join(mainDir, "notes.html")));
+    app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "../public/notes.html")));
 }
