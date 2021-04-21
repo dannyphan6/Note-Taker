a9311b49424edb31abcc699db39c3d4a27dd875c
Daniel Phan <dannyphan@comcast.net>
2021-04-20 23:42:04 -0700

    Fixed issue with app being undefined

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,8 @@
 const path = require("path")
 const mainDir = path.join(__dirname, "/public")
 
-app.get("/", (req, res) => res.sendFile(path.join(mainDir, "index.html")));
-
-app.get("/notes", (req, res) => res.sendFile(path.join(mainDir, "notes.html")));
+module.exports = app => {
+    app.get("/", (req, res) => res.sendFile(path.join(mainDir, "index.html")));
+    
+    app.get("/notes", (req, res) => res.sendFile(path.join(mainDir, "notes.html")));
+}
