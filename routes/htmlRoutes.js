5a7e96fd1b6cf4cf9e3206a78916c89380ce1044
Daniel Phan <dannyphan@comcast.net>
2021-04-25 22:04:06 -0700

    Refactored HTML routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,7 @@
-const path = require("path")
-// const mainDir = path.join(__dirname, "/public")
+const path = require("path");
 
 module.exports = app => {
     app.get("/", (req, res) => res.sendFile(path.join(mainDir, "index.html")));
     
     app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "../public/notes.html")));
-}
+};
